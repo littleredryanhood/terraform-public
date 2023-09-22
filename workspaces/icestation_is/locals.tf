f29aab1b06aef33dafa91c7f40cae126d9095aa4
Little Red Ryan Hood <47164393+littleredryanhood@users.noreply.github.com>
2023-09-22 08:13:30 -0700

    adding spf (#5)

--- workspaces/icestation_is/locals.tf
+++ workspaces/icestation_is/locals.tf
@@ -9,6 +9,12 @@ locals {
       value    = "smtp.google.com."
       priority = "1"
     }
+    "spf_records" = {
+      name  = "@"
+      type  = "TXT"
+      value = "v=spf1 include:_spf.google.com ~all"
+      ttl   = 3600
+    }
   }
 
 
